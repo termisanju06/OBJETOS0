3e26dddb7119caf6f48b09a8738c74e7d7e2ba56
termi <pablo.san14@educa.madrid.org>
2025-10-30 13:36:45 +0100

    objetos

--- src/main/java/org/termi/objetos/highschool/readers/Highschoolreader.java
+++ src/main/java/org/termi/objetos/highschool/readers/Highschoolreader.java
@@ -7,20 +7,25 @@ import java.util.Scanner;
 
 public class Highschoolreader {
     private final Scanner sc;
+    private final Studentreader studentreader;
 
-    public Highschoolreader(Scanner sc) {
+    public Highschoolreader(Scanner sc, Studentreader studentreader) {
         this.sc = sc;
+        this.studentreader = studentreader;
+
     }
-    public Student read() {
+    public Highschool read() {
         System.out.println("Introduce tu nombre");
         String name = sc.nextLine();
 
-        System.out.println("Introduce los estudiantes");
         System.out.println("Cuantos estudiantes hay");
- //       Student[] students = Student[];
-
-
-
-        return ;
+        int a = sc.nextInt();
+        System.out.println("Introduce los estudiantes");
+        Student[] students = new Student[a];
+        for (int i = 0;i<a;i++){
+            students[i] = studentreader.read();
+        }
+        Highschool highschool = new Highschool(name,students);
+        return highschool;
     }
 }
--- src/main/java/org/termi/objetos/highschool/readers/Studentreader.java
+++ src/main/java/org/termi/objetos/highschool/readers/Studentreader.java
@@ -1,5 +1,7 @@
 package org.termi.objetos.highschool.readers;
 
+import org.termi.objetos.highschool.model.Student;
+
 import java.util.Scanner;
 
 public class Studentreader {
@@ -7,5 +9,18 @@ public class Studentreader {
 
     public Studentreader(Scanner sc) {
         this.sc = sc;
+    }
+    public Student read() {
+        System.out.println("Dame tu nombre");
+        String name = sc.nextLine();
+
+        System.out.println("Dame tu apellido");
+        String surname = sc.nextLine();
+
+        System.out.println("Dame tu dirección");
+        String address = sc.nextLine();
+
+        Student student = new Student(name,surname,address);
+        return student;
     }
 }
