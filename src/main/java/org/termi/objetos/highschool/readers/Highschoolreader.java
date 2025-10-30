45c98b8b639147e4025b10dc7633c2e80d8fa434
termi <pablo.san14@educa.madrid.org>
2025-10-30 13:07:33 +0100

    objetos

--- src/main/java/org/termi/objetos/highschool/readers/Highschoolreader.java
+++ src/main/java/org/termi/objetos/highschool/readers/Highschoolreader.java
@@ -1,9 +1,26 @@
 package org.termi.objetos.highschool.readers;
 
 import org.termi.objetos.highschool.model.Highschool;
+import org.termi.objetos.highschool.model.Student;
 
 import java.util.Scanner;
 
 public class Highschoolreader {
+    private final Scanner sc;
 
+    public Highschoolreader(Scanner sc) {
+        this.sc = sc;
+    }
+    public Student read() {
+        System.out.println("Introduce tu nombre");
+        String name = sc.nextLine();
+
+        System.out.println("Introduce los estudiantes");
+        System.out.println("Cuantos estudiantes hay");
+ //       Student[] students = Student[];
+
+
+
+        return ;
+    }
 }
--- src/main/java/org/termi/objetos/highschool/readers/Studentreader.java
+++ src/main/java/org/termi/objetos/highschool/readers/Studentreader.java
@@ -1,4 +1,11 @@
 package org.termi.objetos.highschool.readers;
 
+import java.util.Scanner;
+
 public class Studentreader {
+    private final Scanner sc;
+
+    public Studentreader(Scanner sc) {
+        this.sc = sc;
+    }
 }
